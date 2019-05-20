d49bd8b613750bb8d8b66d72b1da4bb5955237fd
tuananhle1997 <tuananhle250597@gmail.com>
2019-05-21 06:10:46 +0700

    init

---
+++ Arrays/count($array)/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1>hàm này để xem mảng có bao nhiêu phần tử</h1>
+<?php
+    $array = [
+        'toidicode.com','Website học lập trình online'
+    ];
+    echo count($array);
+
+?>
+</body>
+</html>
